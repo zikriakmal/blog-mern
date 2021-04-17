cc6a2c02ea719c3bdd43b28daebb070c4995eb37
zikri <zikriakmale@gmail.com>
2021-04-17 08:02:38 +0700

    detail blog layout and styling

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
-
+import React from 'react';
+import { RegisterBg } from '../../assets';
+import './detailblog.scss';
 const DetailBlog = () => {
     return (
-        <div>
-           <p>Content Detail Blog</p> 
+        <div className="detail-blog-wrapper">
+            <img className="image-cover" src={RegisterBg} alt="thumbnail"/>
+            <p className="blog-title">Title Blog</p> 
+            <p className="blog-subtitle">Author - Post Date</p>
+            <p className="blog-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae risus facilisis, posuere nibh sed, gravida diam. Nunc sagittis et felis vel pulvinar. In laoreet justo lacinia nulla pellentesque, ut semper dolor bibendum. Donec a placerat nisl. Vivamus viverra, risus eget finibus sodales, urna massa malesuada leo, ac ornare erat massa id lectus. Sed a sem ac augue bibendum facilisis a a lectus. Vestibulum faucibus pretium ipsum sit amet iaculis.</p>
         </div>
     )
 }
